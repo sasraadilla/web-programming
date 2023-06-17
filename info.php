59389a04cca5fa88f15aa3e9155a2806e36aeea0
Sasra Adilla <136909086+sasraadilla@users.noreply.github.com>
2023-06-17 21:32:32 +0700

    Add files via upload

---
+++ info.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<p>
+	NIM : <?php
+	print_r($_POST['textnim']);
+	?>
+	</p>
+	<p>
+	Nama : <?php
+	print_r($_POST['textnama']);
+	?>
+		</p>
+	<p>
+	Tempat Lahir : <?php
+	print_r($_POST['texttl']);
+	?>
+		</p>
+	<p>
+	Tanggal Lahir : <?php
+	print_r($_POST['textgl']);
+	?>
+		</p>
+</body>
+</html>
